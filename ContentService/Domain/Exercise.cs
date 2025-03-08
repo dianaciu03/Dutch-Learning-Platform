eeeffa8f0440030e25264af5f1c66b4a697a3701
Diana Ciuperca <dianav.ciuperca@gmail.com>
2025-03-09 00:25:53 +0100

    added logic and controller layers for basic crud functionality for exercises

--- ContentService/Domain/Exercise.cs
+++ ContentService/Domain/Exercise.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ContentService.Helpers;
 using ContentService.Interfaces;
 
 namespace ContentService.Domain
@@ -17,11 +16,11 @@ namespace ContentService.Domain
 
         private readonly IEnumerable<IExerciseComponent> _exerciseComponents;
 
-        public Exercise(int id, List<string> types, string level, IEnumerable<IExerciseComponent> exerciseComponents)
+        public Exercise(int id, List<ExerciseType> types, CEFRLevel level, IEnumerable<IExerciseComponent> exerciseComponents)
         {
             this.Id = id;
-            this.ExerciseTypes = types.Select(type => EnumConverter.ParseExerciseType(type)).ToList();
-            this.Level = EnumConverter.ParseCEFRLevel(level);
+            this.ExerciseTypes = types;
+            this.Level = level;
             this.Grade = 0.0;
             this._exerciseComponents = exerciseComponents;
         }
---
+++ ContentService/Interfaces/IExerciseManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentService.Domain;
+
+namespace ContentService.Interfaces
+{
+    public interface IExerciseManager
+    {
+       void AddExercise(int id, List<ExerciseType> types, CEFRLevel level, IEnumerable<IExerciseComponent> exerciseComponents);
+        Exercise GetExerciseById(int id);
+        List<Exercise> GetAllExercises();
+        bool UpdateExercise(int id, List<ExerciseType> types, CEFRLevel level, IEnumerable<IExerciseComponent> exerciseComponents);
+        bool DeleteExercise(int id);
+    }
+}
